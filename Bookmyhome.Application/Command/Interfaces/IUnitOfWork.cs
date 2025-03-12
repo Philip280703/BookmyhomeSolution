4a40376b99fcf0bf1560960dea77ecf851d1bbc1
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-03-12 20:17:52 +0100

    added UnitOfWork

---
+++ Bookmyhome.Application/Command/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Bookmyhome.Application.Command.Interfaces
+{
+	public interface IUnitOfWork
+	{
+		void Commit();
+		void Rollback();
+		void BeginTransaction(System.Data.IsolationLevel isolationLevel = System.Data.IsolationLevel.Serializable);
+	}
+}
---
+++ Bookmyhome.Application/Command/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Bookmyhome.Application.Command.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data;
+
+namespace Bookmyhome.Application.Command
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly DbContext _db;
+		private IDbContextTransaction? _transaction;
+
+		public UnitOfWork (DbContext db)
+		{
+			_db = db;
+		}
+
+		void IUnitOfWork.BeginTransaction(System.Data.IsolationLevel isolationLevel = System.Data.IsolationLevel.Serializable)
+		{
+			if (_db.Database.CurrentTransaction != null) return;
+			_transaction = _db.Database.BeginTransaction(isolationLevel);
+		}
+		void IUnitOfWork.Commit()
+		{
+			if (_transaction == null) throw new Exception("You must call 'BeginTransaction' before Commit is called");
+			_transaction.Commit();
+			_transaction.Dispose();
+		}
+		void IUnitOfWork.Rollback()
+		{
+			if (_transaction == null) throw new Exception("You must call 'BeginTransaction' before Rollback is called");
+			_transaction.Rollback();
+			_transaction.Dispose();
+		}
+
+	}
+}
