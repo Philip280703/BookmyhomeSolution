8b9ff8422c225b99a8dd86310b76b5e29dafc820
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-03-20 12:35:17 +0100

    non git ignore more

---
+++ Bookmyhome.Persistence/ContextDB/ConnString.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Bookmyhome.Persistence.ContextDB
+{
+	public static class ConnString
+	{
+		private static IConfiguration _configuration;
+		public static void Initialize(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public static string GetConnString()
+		{
+			return	_configuration.GetConnectionString("DefaultConnection");
+		}
+	}
+}
